cce867869bad82f0932a9cfb620646856c950a04
Gökmen Görgen <gkmngrgn@gmail.com>
2024-08-05 00:42:47 +0200

    add cli arg.

--- kaos_py/kaos_py/__init__.py
+++ kaos_py/kaos_py/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Final
 
 from kaos_py.backend_bmp import backend_bmp
@@ -9,9 +10,17 @@ WIDTH: Final = 800
 HEIGHT: Final = 800
 
 
-def main() -> None:
+def cli() -> None:
     world = Rectangle2D(-1.08, -1.08, 1.08, 1.08)
-    selection = 0
+
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        selection = int(sys.argv[1])
+    else:
+        selection = 0
+
+    print(f"Selection is: {selection}")
     points = generate_points(KaosGame.max_iterations, selection=selection)
     screen_space = Rectangle2D(left=0, bottom=0, right=WIDTH - 1, top=HEIGHT - 1)
-    backend_bmp(f"kaos_{selection}.bmp", WIDTH, HEIGHT, world, screen_space, points, 0)
+    file_name = f"kaos_{selection}.bmp"
+    backend_bmp(file_name, WIDTH, HEIGHT, world, screen_space, points, 0)
+    print(f"Saved to: {file_name}")
