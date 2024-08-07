ac0e19d2d36389ddcdec16efb7be6ab31adfc741
Gökmen Görgen <gkmngrgn@gmail.com>
2024-08-08 00:17:57 +0200

    complete `generate_points`.

--- kaos_go/kaos.go
+++ kaos_go/kaos.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
 
 const (
-	WIDTH  = 800
-	HEIGHT = 800
+	WIDTH          = 800
+	HEIGHT         = 800
+	MAX_ITERATIONS = 100000
 )
 
 type Point2D struct {
@@ -25,25 +28,23 @@ type RegularPolygon struct {
 	Radius     float64
 	StartAngle float64
 	Angle      float64
-	Points     [][]float64
+	Points     []Point2D
 }
 
 func NewRegularPolygon(nrEdges int, radius float64, startAngle float64) *RegularPolygon {
+	angle := 360.0 / float64(nrEdges)
+
+	// We change the start_angle in order to have the lower edge of every polygon parallel with the horizontal axis.
+	if nrEdges%2 == 0 {
+		startAngle += angle / 2.0
+	}
+
 	rp := &RegularPolygon{
 		NrEdges:    nrEdges,
 		Radius:     radius,
 		StartAngle: startAngle,
+		Angle:      angle,
 	}
-	rp.Angle = 360.0 / float64(nrEdges)
-	rp.Points = make([][]float64, nrEdges)
-	for i := range rp.Points {
-		rp.Points[i] = make([]float64, 2)
-	}
-
-	if nrEdges%2 == 0 {
-		rp.StartAngle += rp.Angle / 2.0
-	}
-
 	rp.initPoints()
 	return rp
 }
@@ -54,117 +55,170 @@ func (rp *RegularPolygon) initPoints() {
 	minY := 2.0
 
 	for index := 0; index < rp.NrEdges; index++ {
-		point := Point2D{
+		rp.Points[index] = Point2D{
 			X: rp.Radius * math.Cos(currentAngle),
 			Y: rp.Radius * math.Sin(currentAngle),
 		}
 
-		rp.Points[index][0] = point.X
-		rp.Points[index][1] = point.Y
-
-		if minY > point.Y {
-			minY = point.Y
+		if minY > rp.Points[index].Y {
+			minY = rp.Points[index].Y
 		}
 
 		currentAngle += rp.Angle * degRad
 	}
 }
 
-func generate_points(max_iterations int, selection int) []Point2D {
+type KaosGame struct {
+	Polygon               RegularPolygon
+	LastPoint             Point2D
+	LastVertex            int
+	MaxIterations         int
+	IgnoreFirstIterations int
+}
+
+func NewKaosGame(polygon RegularPolygon) *KaosGame {
+	return &KaosGame{
+		Polygon:               polygon,
+		LastPoint:             polygon.Points[0],
+		LastVertex:            0,
+		MaxIterations:         MAX_ITERATIONS,
+		IgnoreFirstIterations: 10,
+	}
+}
+
+func (kg *KaosGame) GetNextPoint(
+	fn func(int, int, int) bool,
+	ratio float64,
+	dist int,
+) Point2D {
+	running := true
+	removeIter := 0
+
+	rand.Seed(time.Now().UnixNano())
+
+	for running {
+		randomVertex := rand.Intn(len(kg.Polygon.Points))
+
+		if fn(randomVertex, kg.LastVertex, dist) {
+			randomPoint := kg.Polygon.Points[randomVertex]
+			point := Point2D{
+				X: (kg.LastPoint.X + randomPoint.X) * ratio,
+				Y: (kg.LastPoint.Y + randomPoint.Y) * ratio,
+			}
+			kg.LastVertex = randomVertex
+			kg.LastPoint = point
+			removeIter++
+
+			if removeIter >= kg.IgnoreFirstIterations {
+				running = false
+			}
+		}
+	}
+
+	return kg.LastPoint
+}
+
+func isPointValid(randomVertex, lastVertex, dist int) bool {
+	return math.Abs(float64(randomVertex-lastVertex)) != float64(dist)
+}
+
+func isPointValid1(randomVertex, lastVertex, dist int) bool {
+	return true
+}
+
+func generate_points(maxIterations int, selection int) []Point2D {
 	var points []Point2D
+	var isValidFn func(int, int, int) bool
 	var nrEdges int
+	var ratio float64
+	var distance int
 
 	switch selection {
 	case 1:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 4
-		// ratio = 0.5
-		// distance = 0
+		ratio = 0.5
+		distance = 0
 	case 2:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 4
-		// ratio = 0.5
-		// distance = 2
+		ratio = 0.5
+		distance = 2
 	case 3:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 5
-		// ratio = 0.5
-		// distance = 0
-
+		ratio = 0.5
+		distance = 0
 	case 4:
-		// func = is_valid_point_1
+		isValidFn = isPointValid1
 		nrEdges = 7
-		// ratio = 0.4
-		// distance = 0
-
+		ratio = 0.4
+		distance = 0
 	case 5:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 7
-		// ratio = 0.4
-		// distance = 3
-
+		ratio = 0.4
+		distance = 3
 	case 6:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 6
-		// ratio = 0.4
-		// distance = 3
-
+		ratio = 0.4
+		distance = 3
 	case 7:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 6
-		// ratio = 0.375
-		// distance = 0
-
+		ratio = 0.375
+		distance = 0
 	case 8:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 6
-		// ratio = 0.5
-		// distance = 2
-
+		ratio = 0.5
+		distance = 2
 	case 9:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 8
-		// ratio = 0.4
-		// distance = 0
-
+		ratio = 0.4
+		distance = 0
 	case 10:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 10
-		// ratio = 0.375
-		// distance = 1
-
+		ratio = 0.375
+		distance = 1
 	case 11:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 10
-		// ratio = 0.375
-		// distance = 2
-
+		ratio = 0.375
+		distance = 2
 	case 12:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 10
-		// ratio = 0.375
-		// distance = 3
-
+		ratio = 0.375
+		distance = 3
 	case 13:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 10
-		// ratio = 0.375
-		// distance = 4
-
+		ratio = 0.375
+		distance = 4
 	case 14:
-		// func = is_valid_point
+		isValidFn = isPointValid
 		nrEdges = 10
-		// ratio = 0.375
-		// distance = 5
-
+		ratio = 0.375
+		distance = 5
 	default:
-		// func = is_valid_point_1
+		isValidFn = isPointValid1
 		nrEdges = 3
-		// ratio = 0.5
-		// distance = 0
+		ratio = 0.5
+		distance = 0
 	}
 
 	polygon := NewRegularPolygon(nrEdges, 1.0, 90.0)
+	kaos := NewKaosGame(*polygon)
+
+	for i := 0; i < maxIterations; i++ {
+		point := kaos.GetNextPoint(isValidFn, ratio, distance)
+		points = append(points, point)
+	}
+
 	return points
 }
 
@@ -183,8 +237,8 @@ func main() {
 	}
 
 	fmt.Printf("Selection is: %d\n", selection)
-
-	file_name := fmt.Sprintf("kaos_%d.bmp", selection)
-	// points = generate_points(KaosGame.max_iterations, selection=selection)
-	fmt.Printf("Saving to: %s\n", file_name)
+	// points := generate_points(MAX_ITERATIONS, selection)
+	// screenSpace := Rectangle2D{Left: 0, Bottom: 0, Right: WIDTH - 1, Top: HEIGHT - 1}
+	fileName := fmt.Sprintf("kaos_%d.bmp", selection)
+	fmt.Printf("Saved to: %s\n", fileName)
 }
