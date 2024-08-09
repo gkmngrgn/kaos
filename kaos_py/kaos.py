4864948303c94572c7ece8830796ec711dc31117
Gökmen Görgen <gkmngrgn@gmail.com>
2024-08-09 20:16:09 +0200

    merge two image functions.

--- kaos_py/kaos.py
+++ kaos_py/kaos.py
@@ -91,45 +91,6 @@ def points_to_screen_space(
     return [wssp.mapping(p) for p in points]
 
 
-def draw_point_with_size(
-    image: Image.Image, width: int, height: int, point: Point2D, radius: int
-) -> None:
-    "Draw a `crude` circle around the point."
-    radius2 = radius**2
-    xmin = max(int(point.x - radius), 0)
-    xmax = min(int(point.x + radius), width - 1)
-    ymin = max(int(point.y - radius), 0)
-    ymax = min(int(point.y + radius), height - 1)
-
-    for j in range(ymin, ymax + 1):
-        for i in range(xmin, xmax + 1):
-            dist = int((i - point.x) ** 2 + (j - point.y) ** 2)
-            if dist <= radius2:
-                image.putpixel(xy=(i, j), value=(255, 0, 0))
-
-
-def backend_bmp(
-    file_name: str,
-    width: int,
-    height: int,
-    world: Rectangle2D,
-    screen_space: Rectangle2D,
-    points: list[Point2D],
-    point_radius: int,
-) -> None:
-    image = Image.new("RGB", (width, height), color="white")
-    points = points_to_screen_space(world, screen_space, points)
-
-    if point_radius == 0:
-        for point in points:
-            image.putpixel(xy=(int(point.x), int(point.y)), value=(255, 0, 0))
-    else:
-        for point in points:
-            draw_point_with_size(image, width, height, point, point_radius)
-
-    image.save(file_name)
-
-
 class KaosGame:
     max_iterations = 100_000
     ignore_first_iterations = 10
@@ -275,6 +236,38 @@ def generate_points(max_iterations: int, selection: int = 0) -> list[Point2D]:
         points.append(kaos.get_next_point(func, ratio, distance))
 
     return points
+
+
+def backend_bmp(
+    file_name: str,
+    width: int,
+    height: int,
+    world: Rectangle2D,
+    screen_space: Rectangle2D,
+    points: list[Point2D],
+    point_radius: int,
+) -> None:
+    image = Image.new("RGB", (width, height), color="white")
+    points = points_to_screen_space(world, screen_space, points)
+    radius2 = point_radius**2
+
+    if point_radius == 0:
+        for point in points:
+            image.putpixel(xy=(int(point.x), int(point.y)), value=(255, 0, 0))
+    else:
+        for point in points:
+            xmin = max(int(point.x - point_radius), 0)
+            xmax = min(int(point.x + point_radius), width - 1)
+            ymin = max(int(point.y - point_radius), 0)
+            ymax = min(int(point.y + point_radius), height - 1)
+
+            for j in range(ymin, ymax + 1):
+                for i in range(xmin, xmax + 1):
+                    dist = int((i - point.x) ** 2 + (j - point.y) ** 2)
+                    if dist <= radius2:
+                        image.putpixel(xy=(i, j), value=(255, 0, 0))
+
+    image.save(file_name)
 
 
 def main() -> None:
